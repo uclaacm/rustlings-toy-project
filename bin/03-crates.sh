f8952d52ee86392497ee134cc7097f4987255923
Matt Wang <matt@matthewwang.me>
2022-06-18 23:35:14 -0700

    renames 02-simple-cli -> 03-crates; creates new simple CLI fixture

--- bin/03-crates.sh
+++ bin/03-crates.sh
@@ -2,8 +2,8 @@
 
 source $(dirname "$0")/harness.sh
 
-test_name='02_simple_cli_test_file'
-path_to_expected_resp=$(dirname "$0")/fixtures/02_simple_cli_expected
+test_name='03_crates_bee_movie_test'
+path_to_expected_resp=$(dirname "$0")/fixtures/03_crates_bee_movie_expected
 test_opts=$(dirname "$0")/fixtures/bee-movie-script.txt
 
 run_and_compare $test_name $path_to_expected_resp $test_opts
